5abe2a0580b8ad6c5d4e36c9faa4a9945d8b3429
aoirint <aoirint@gmail.com>
2021-09-03 07:10:55 +0900

    docstring

--- jasmine_zinc/Talk.py
+++ jasmine_zinc/Talk.py
@@ -3,21 +3,42 @@ from typing import Dict, Any
 
 @dataclass(frozen=True)
 class TalkEffects:
-    volume: float = 1 # 0-2, step 0.01
-    speed: float = 1 # 0.5-4, step 0.01
-    pitch: float = 1 # 0.5-2, step 0.01
-    intonation: float = 1 # 0-2, step 0.01
-    shortpause: int = 150 # 80-500, step 1
-    longpause: int = 370 # 100-2000, step 1
+    """
+        Args:
+            volume (float): 0-2, step 0.01
+            speed (float): 0.5-4, step 0.01
+            pitch (float): 0.5-2, step 0.01
+            intonation (float): 0-2, step 0.01
+            shortpause (int): 80-500, step 1
+            longpause (int): 100-2000, step 1
+    """
+    volume: float = 1
+    speed: float = 1
+    pitch: float = 1
+    intonation: float = 1
+    shortpause: int = 150
+    longpause: int = 370
 
 @dataclass(frozen=True)
 class TalkEmotions:
-    joy: float = 0 # 0-1, step 0.01
-    anger: float = 0 # 0-1, step 0.01
-    sadness: float = 0 # 0-1, step 0.01
+    """
+        Args:
+            joy (float): 0-1, step 0.01
+            anger (float): 0-1, step 0.01
+            sadness (float): 0-1, step 0.01
+    """
+    joy: float = 0
+    anger: float = 0
+    sadness: float = 0
 
 @dataclass
 class Talk:
+    """
+        Args:
+            talktext (str):
+            effects (TalkEffects):
+            emotions (TalkEmotions):
+    """
     talktext: str
     effects: TalkEffects = TalkEffects()
     emotions: TalkEmotions = TalkEmotions()
